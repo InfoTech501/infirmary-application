423884e9d1e4e02c60d284b6d8c39cd2c8591afb
Noaflores <noahelijahflores07@gmail.com>
2025-02-15 23:36:32 +0800

    Code review - Create Medical Records

---
+++ src/main/java/com/rocs/medical/records/application/app/facade/MedicalRecords/impl/MedicalRecordsFacadeImpl.java
@@ -0,0 +1,19 @@
+package com.rocs.medical.records.application.app.facade.MedicalRecords.impl;
+
+import com.rocs.medical.records.application.app.facade.MedicalRecords.MedicalRecordsFacade;
+import com.rocs.medical.records.application.data.dao.MedicalRecordsDao;
+import com.rocs.medical.records.application.data.dao.impl.medicalrecordsDaoImpl;
+import com.rocs.medical.records.application.model.MedicalRecords.MedicalRecords;
+
+
+public class MedicalRecordsFacadeImpl implements MedicalRecordsFacade {
+
+        private MedicalRecordsDao medicalRecordsDao = new medicalrecordsDaoImpl();
+
+        @Override
+        public boolean patient(MedicalRecords medicalRecords) {
+            return this.medicalRecordsDao.patient(medicalRecords);
+        }
+
+}
+
---
+++ src/main/java/com/rocs/medical/records/application/data/dao/commonAilmentsReport/MedicalRecordsDao.java
@@ -0,0 +1,9 @@
+package com.rocs.medical.records.application.data.dao;
+
+import com.rocs.medical.records.application.model.MedicalRecords.MedicalRecords;
+
+
+public interface MedicalRecordsDao {
+
+    boolean patient(MedicalRecords MedicalRecords);
+}
