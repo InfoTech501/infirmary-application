7bba5a5253ba6eba2607bb313c0279cc12dcc3a0
juliuspatrickpeji <juliuspatrick.amita.peji@gmail.com>
2025-02-22 23:07:52 +0800

    peji commit

---
+++ src/main/java/com/rocs/medical/records/application/data/dao/medicineInventory/InventoryDao.java
@@ -0,0 +1,11 @@
+package com.rocs.medical.records.application.data.dao.medicineInventory;
+
+import com.rocs.medical.records.application.model.Inventory.Inventory;
+
+import java.util.List;
+
+public interface InventoryDao {
+
+
+    List<Inventory> getInventoryItems();
+}
