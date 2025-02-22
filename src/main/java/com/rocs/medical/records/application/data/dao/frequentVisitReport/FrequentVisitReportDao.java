cfc7f5fd686ef03a66e0b0066885a5adcbda01de
kim <kim.yashai.legaspi.panlilio@gmail.com>
2025-02-22 23:07:20 +0800

    frequent visit documentation

--- src/main/java/com/rocs/medical/records/application/data/dao/frequentVisitReport/FrequentVisitReportDao.java
+++ src/main/java/com/rocs/medical/records/application/data/dao/frequentVisitReport/FrequentVisitReportDao.java
@@ -5,13 +5,15 @@ import com.rocs.medical.records.application.model.reports.FrequentVisitReport;
 import java.util.Date;
 import java.util.List;
 
-/**
- * * This returns the FrequentVisitReport List with the given report period.
- * * @param startDate -  The start of the report date period.
- * * @param endDate - The end of the report date period.
- * * @param gradeLevel - The grade level of the students with frequent visit.
- * */
-
 public interface FrequentVisitReportDao {
+
+    /**
+     * * This returns the generated report of FrequentVisitReport List with the given report period.
+     * * @param startDate -  The start of the report date period.
+     * * @param endDate - The end of the report date period.
+     * * @param gradeLevel - The grade level of the students with frequent visit.
+     * * @return list of FrequentVisitReport object like student id, firstName, lastName, grade level, symptoms, visit count, and visit date.
+     * */
+
     List<FrequentVisitReport> getFrequentVisitReports(String gradeLevel, Date startDate, Date endDate);
 }
