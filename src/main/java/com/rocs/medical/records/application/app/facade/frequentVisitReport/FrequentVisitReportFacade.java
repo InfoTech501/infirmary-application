08b76e1b09d90c804aace47c47d985cec850b2a5
KYPanlilio <kim.yashai.legaspi.panlilio@gmail.com>
2025-02-23 21:07:02 +0800

    Update FrequentVisitReportFacade.java
    revise the previous documentation

--- src/main/java/com/rocs/medical/records/application/app/facade/frequentVisitReport/FrequentVisitReportFacade.java
+++ src/main/java/com/rocs/medical/records/application/app/facade/frequentVisitReport/FrequentVisitReportFacade.java
@@ -32,12 +32,5 @@ public interface FrequentVisitReportFacade {
      * * @return list of FrequentVisitReport object like student id, firstName, lastName, grade level, symptoms, visit count, and visit date.
      * */
 
-    /**
-     * This is an interface that manages the report of the frequent visit of the student in the clinic within the given period of date and grade level
-     * * @param startDate
-     * *@param endDate
-     * *@return list of FrequentVisitReport like student id, student first and last name,grade level, symptoms,visit count, visit date.
-     * */
-
     void handleFrequentVisit(List<FrequentVisitReport> reports, Date startDate, Date endDate, String gradeLevel);
 }
