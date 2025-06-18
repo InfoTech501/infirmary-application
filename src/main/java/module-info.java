66243d9b807f783b0ca45d13b65686038a5f2019
M-Anacay <mary.ann.jacinto.anacay@gmail.com>
2025-06-19 03:50:36 +0800

    fixed dashboard page controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,8 @@ module com.rocs.infirmaryapplication {
     opens com.rocs.infirmary.application to javafx.fxml;
     opens com.rocs.infirmary.application.data.model.inventory.medicine to javafx.base;
     opens com.rocs.infirmary.application.controller.mainpage to javafx.fxml;
+    opens com.rocs.infirmary.application.controller.dashboard to javafx.fxml;
+    opens com.rocs.infirmary.application.data.model.report to javafx.fxml;
 
     exports com.rocs.infirmary.application;
     exports com.rocs.infirmary.application.data.model.inventory;
@@ -18,4 +20,6 @@ module com.rocs.infirmaryapplication {
     exports com.rocs.infirmary.application.module.inventory.management.application;
     opens com.rocs.infirmary.application.module.inventory.management.application to javafx.fxml;
     exports com.rocs.infirmary.application.controller.mainpage;
+    exports com.rocs.infirmary.application.controller.dashboard;
+    exports com.rocs.infirmary.application.data.model.report;
 }
