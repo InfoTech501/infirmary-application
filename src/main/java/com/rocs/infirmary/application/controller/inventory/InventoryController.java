e0cbf33453f163b7336c302cab997b40c2c215a4
NNeonael <neonael.angcaya.nazareno@gmail.com>
2025-06-27 19:45:26 +0800

    enabled modal display on click

--- src/main/java/com/rocs/infirmary/application/controller/inventory/InventoryController.java
+++ src/main/java/com/rocs/infirmary/application/controller/inventory/InventoryController.java
@@ -4,7 +4,6 @@ import com.rocs.infirmary.application.LowStockNotificationServiceApplication;
 import com.rocs.infirmary.application.data.model.inventory.medicine.Medicine;
 import com.rocs.infirmary.application.InventoryManagementApplication;
 import com.rocs.infirmary.application.data.model.report.lowstock.LowStockReport;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -13,17 +12,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -31,7 +25,6 @@ import javafx.stage.*;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -58,9 +51,14 @@ public class InventoryController implements Initializable {
     private List<Medicine> medicineList = new ArrayList<>();
 
     private LowStockNotificationServiceApplication lowStockNotificationServiceApplication = new LowStockNotificationServiceApplication();
-     private List<LowStockReport> lowStockItems = new ArrayList<>();
+    private List<LowStockReport> lowStockItems = new ArrayList<>();
+    private boolean lowLowStock = false;
 
 
+    @FXML
+    private ImageView RedCircle;
+    @FXML
+    private ImageView NotificationImage;
 
 
     @Override
@@ -232,15 +230,19 @@ public class InventoryController implements Initializable {
             controller.setAlertDetails("Low Stock Alert", message);
 
             Scene scene = new Scene(root);
+            scene.setFill(Color.TRANSPARENT);
+
             Stage modalStage = new Stage();
+
             modalStage.setScene(scene);
             modalStage.initOwner(ownerStage);
             modalStage.initModality(Modality.NONE);
-            modalStage.initStyle(StageStyle.UNDECORATED);
-            modalStage.setAlwaysOnTop(true);
+            modalStage.initStyle(StageStyle.TRANSPARENT);
 
-            double x = ownerStage.getX() + ownerStage.getWidth() - 472 - 20;
+
+            double x = ownerStage.getX() + ownerStage.getWidth() - 472 - 60;
             double y = ownerStage.getY() + ownerStage.getHeight() - 580 - 20;
+
             modalStage.setX(x);
             modalStage.setY(y);
 
@@ -260,11 +262,16 @@ public class InventoryController implements Initializable {
                     .map(LowStockReport::getDescription)
                     .toList();
 
-            Platform.runLater(() -> {
-                Stage stage = (Stage) MedDetailsTable.getScene().getWindow();
-                showLowStockModal(stage, productInfo);
-            });
+                RedCircle.setVisible(true);
+                NotificationImage.setOnMouseClicked(event -> {
+                    Stage stage = (Stage) MedDetailsTable.getScene().getWindow();
+                    showLowStockModal(stage, productInfo);
+                });
+
+             lowLowStock = true;
         }
+
+
     }
 }
 
--- src/main/java/com/rocs/infirmary/application/controller/inventory/LowStockNotificationController.java
+++ src/main/java/com/rocs/infirmary/application/controller/inventory/LowStockNotificationController.java
@@ -1,11 +1,20 @@
 package com.rocs.infirmary.application.controller.inventory;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
-public class LowStockNotificationController  {
+
+
+public class LowStockNotificationController {
 
     @FXML
     private VBox AlertContainer;
@@ -16,10 +25,21 @@ public class LowStockNotificationController  {
     @FXML
     private Label AlertMessage;
 
+
+
+
     public void setAlertDetails(String title, String message) {
         AlertTitle.setText(title);
         AlertMessage.setText(message);
         AlertContainer.setVisible(true);
         AlertContainer.setManaged(true);
     }
+
+    @FXML
+    private void onCloseButton(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
 }
+
+
