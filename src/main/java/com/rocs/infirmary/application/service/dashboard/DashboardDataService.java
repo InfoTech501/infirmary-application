12224823f53345efabde85a7c2564b5ded6db9ed
M-Anacay <mary.ann.jacinto.anacay@gmail.com>
2025-07-15 18:40:29 +0800

    fixing

--- src/main/java/com/rocs/infirmary/application/service/dashboard/DashboardDataService.java
+++ src/main/java/com/rocs/infirmary/application/service/dashboard/DashboardDataService.java
@@ -6,19 +6,31 @@ import com.rocs.infirmary.application.data.model.report.visit.FrequentVisitRepor
 
 import java.util.List;
 
+/**
+ * This class DashboardDataService gets the data to display on the Dashboard Page.
+ * */
 public class DashboardDataService {
     private final DashboardFacade dashboardFacade;
-
+    /**
+     * This gets the Dashboard Facade.
+     * @return the dashboard facade.
+     */
     public DashboardDataService(DashboardFacade dashboardFacade) {
         this.dashboardFacade = dashboardFacade;
     }
-
+    /**
+     * This gets the visit count to the generated frequent visit report from dashboardFacade.
+     * @return the size of the generated visit report.
+     */
     public int getVisitCount (DateRange dateRange, String gradeLevel) {
         List<FrequentVisitReport> reports = dashboardFacade.generateFrequentVisitReport(
                 dateRange.getStartDate(), dateRange.getEndDate(), gradeLevel);
         return reports.size();
     }
-
+    /**
+     * This gets the total medication usage to the generated medication report from dashboardFacade.
+     * @return the summation of all medication trend report.
+     */
     public int getTotalMedicationUsage(DateRange dateRange) {
         List<MedicationTrendReport> reports = dashboardFacade.generateMedicationReport(
                 dateRange.getStartDate(), dateRange.getEndDate());
