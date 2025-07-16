fe2b36a6e3c17e24cbaf6f858dfa3c495541fb8f
Arsssie <maria.arssiely.delmundo@gmail.com>
2025-07-16 22:29:50 +0800

    Disabled added

--- src/test/java/com/rocs/infirmary/application/controller/mainpage/MainpageControllerTest.java
+++ src/test/java/com/rocs/infirmary/application/controller/mainpage/MainpageControllerTest.java
@@ -14,6 +14,7 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
+import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(ApplicationExtension.class)
@@ -27,7 +28,7 @@ class MainpageControllerTest {
     private Button studentHealthProfileBtn;
 
     @Start
-    private void start(Stage stage) throws Exception {
+    private void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Mainpage.fxml"));
         BorderPane mainLayout;
         mainLayout = loader.load();
