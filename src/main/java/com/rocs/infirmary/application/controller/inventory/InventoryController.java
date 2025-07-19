b4f245b79b5728c4f74cb687b46f02e2b61debd1
NNeonael <neonael.angcaya.nazareno@gmail.com>
2025-07-20 04:40:17 +0800

    clean

--- src/main/java/com/rocs/infirmary/application/controller/inventory/InventoryController.java
+++ src/main/java/com/rocs/infirmary/application/controller/inventory/InventoryController.java
@@ -19,7 +19,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Timestamp;
