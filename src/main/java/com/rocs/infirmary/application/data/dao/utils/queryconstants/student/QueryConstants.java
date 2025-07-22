e6bb03edfee1d9a324fb01ea756ae2125cc7254c
jakeeviado <jakematthewviado204@gmail.com>
2025-07-23 03:52:29 +0800

    fix query to retrieve health profile even if no medical records

--- src/main/java/com/rocs/infirmary/application/data/dao/utils/queryconstants/student/QueryConstants.java
+++ src/main/java/com/rocs/infirmary/application/data/dao/utils/queryconstants/student/QueryConstants.java
@@ -75,17 +75,20 @@ public class QueryConstants {
 
     public static final String SELECT_STUDENT_HEALTH_PROFILE_QUERY = "SELECT * FROM (" +
             "  SELECT " +
-            "    mr.is_active, p.first_name, p.middle_name, p.last_name, p.gender, p.age, p.contact_number, p.birthdate, p.address, " +
-            "    section.section, student.lrn, section.grade_level, adviser.first_name AS adviser_first_name, " +
-            "    mr.visit_date, nurse.first_name AS NURSE_IN_CHARGE, nurse.last_name AS NURSE_LAST_NAME, " +
-            "    ROW_NUMBER() OVER (PARTITION BY student.id ORDER BY mr.visit_date DESC) rn " +
-            "  FROM MEDICAL_RECORD mr " +
-            "  JOIN PERSON p ON mr.STUDENT_ID = p.ID " +
-            "  JOIN STUDENT ON mr.STUDENT_ID = student.ID " +
+            "    COALESCE(mr.is_active, 0) as is_active, " +
+            "    p.first_name, p.middle_name, p.last_name, p.gender, p.age, p.contact_number, p.birthdate, p.address, " +
+            "    section.section, student.lrn, section.grade_level, " +
+            "    adviser.first_name AS adviser_first_name, " +
+            "    mr.visit_date, " +
+            "    nurse.first_name AS NURSE_IN_CHARGE, " +
+            "    nurse.last_name AS NURSE_LAST_NAME, " +
+            "    ROW_NUMBER() OVER (PARTITION BY student.id ORDER BY COALESCE(mr.visit_date, TO_DATE('1900-01-01', 'YYYY-MM-DD')) DESC) rn " +
+            "  FROM STUDENT " +
+            "  JOIN PERSON p ON student.PERSON_ID = p.ID " +
             "  JOIN SECTION ON student.SECTION_SECTION_ID = section.SECTION_ID " +
-            "  JOIN PERSON nurse ON mr.nurse_in_charge_id = nurse.id " +
             "  LEFT JOIN PERSON adviser ON section.ADVISER_ID = adviser.ID " +
-            "  WHERE mr.is_active = 1" +
+            "  LEFT JOIN MEDICAL_RECORD mr ON student.ID = mr.STUDENT_ID AND mr.is_active = 1 " +
+            "  LEFT JOIN PERSON nurse ON mr.nurse_in_charge_id = nurse.id " +
             ") WHERE rn = 1";
 
     public static final String SELECT_STUDENT_HEALTH_PROFILE_BY_LRN = "SELECT mr.is_active, p.first_name, p.middle_name,p.last_name,p.contact_number,p.email,p.address,mr.symptoms,mr.temperature_readings,visit_date,nurse.first_name as NURSE_FIRST_NAME, nurse.last_name as NURSE_LAST_NAME,mr.treatment\n" +
