302e1aa9ada2ba48ab2b9d960b35d591e066e34d
jakeeviado <jakematthewviado204@gmail.com>
2025-07-25 09:48:44 +0800

    query constants documentation

--- src/main/java/com/rocs/infirmary/application/data/dao/utils/queryconstants/student/QueryConstants.java
+++ src/main/java/com/rocs/infirmary/application/data/dao/utils/queryconstants/student/QueryConstants.java
@@ -1,7 +1,13 @@
 package com.rocs.infirmary.application.data.dao.utils.queryconstants.student;
 
+/**
+ * the {@code QueryConstants} class handles the static method for database queries
+ * */
 public class QueryConstants {
 
+    /**
+     * query that retrieves all active student medical records using lrn
+     * */
     public static final String GET_ALL_ACTIVE_MEDICAL_INFORMATION_BY_LRN = "SELECT " +
             "mr.id AS medical_record_id, " +
             "s.id AS student_id, " +
@@ -24,6 +30,9 @@ public class QueryConstants {
             "INNER JOIN medical_record mr ON s.id = mr.student_id " +
             "WHERE mr.is_active = 1 AND s.LRN = ?";
 
+    /**
+     * query that retrieves all active student medical records using medical records id
+     * */
     public static final String GET_MEDICAL_INFORMATION_BY_ID = "SELECT " +
             "mr.id AS medical_record_id, " +
             "s.id AS student_id, " +
@@ -46,6 +55,9 @@ public class QueryConstants {
             "INNER JOIN medical_record mr ON s.id = mr.student_id " +
             "WHERE mr.is_active = 1 AND mr.id = ?";
 
+    /**
+     * query that retrieves all student medical records
+     * */
     public static String GET_ALL_STUDENTS_MEDICAL_RECORDS = "SELECT " +
             "student.id, " +
             "person.first_name, " +
@@ -63,16 +75,35 @@ public class QueryConstants {
             "LEFT JOIN student ON medical_record.student_id = student.id";
 
 
+    /**
+     * query that deactivates the active student medical record
+     * */
     public static final String DELETE_STUDENT_MEDICAL_RECORD = "UPDATE MEDICAL_RECORD SET IS_ACTIVE = 0 WHERE ID = ?";
 
+    /**
+     * query that updates symptoms of an active student medical record
+     * */
     public static final String UPDATE_STUDENT_SYMPTOMS = "UPDATE MEDICAL_RECORD mr SET mr.SYMPTOMS = ? WHERE mr.ID = ?";
 
+    /**
+     * query that updates temperature of an active student medical record
+     * */
     public static final String UPDATE_STUDENT_TEMPERATURE_READINGS = "UPDATE MEDICAL_RECORD mr SET mr.TEMPERATURE_READINGS = ? WHERE mr.ID = ?";
 
+    /**
+     * query that updates visit date on active student medical record
+     * */
     public static final String UPDATE_STUDENT_VISIT_DATE = "UPDATE MEDICAL_RECORD mr SET mr.VISIT_DATE = ? WHERE mr.ID = ?";
 
+    /**
+     * query that updates treatment on active student medical record
+     * */
     public static final String UPDATE_STUDENT_TREATMENT = "UPDATE MEDICAL_RECORD mr SET mr.TREATMENT = ? WHERE mr.ID = ?";
 
+    /**
+     * query that retrieves all student health profile with their most recent medical record.
+     * the students without medical records will still appear.
+     */
     public static final String SELECT_STUDENT_HEALTH_PROFILE_QUERY = "SELECT * FROM (" +
             "  SELECT " +
             "    COALESCE(mr.is_active, 0) as is_active, " +
@@ -91,6 +122,9 @@ public class QueryConstants {
             "  LEFT JOIN PERSON nurse ON mr.nurse_in_charge_id = nurse.id " +
             ") WHERE rn = 1";
 
+    /**
+     * query that retrieves all student health profile using lrn
+     * */
     public static final String SELECT_STUDENT_HEALTH_PROFILE_BY_LRN = "SELECT mr.is_active, p.first_name, p.middle_name,p.last_name,p.contact_number,p.email,p.address,mr.symptoms,mr.temperature_readings,visit_date,nurse.first_name as NURSE_FIRST_NAME, nurse.last_name as NURSE_LAST_NAME,mr.treatment\n" +
             "FROM MEDICAL_RECORD mr\n" +
             "JOIN PERSON p ON mr.STUDENT_ID = p.ID\n" +
