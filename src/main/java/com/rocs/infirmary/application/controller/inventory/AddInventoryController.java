1b12ae0d7a084c057f1ed3e9847c4dfa5764a02b
NNeonael <neonael.angcaya.nazareno@gmail.com>
2025-08-13 20:18:05 +0800

    clean

--- src/main/java/com/rocs/infirmary/application/controller/inventory/AddInventoryController.java
+++ src/main/java/com/rocs/infirmary/application/controller/inventory/AddInventoryController.java
@@ -293,16 +293,6 @@ public class AddInventoryController implements Initializable {
     private boolean deleteMedicine(){
         return inventoryManagementApplication.getMedicineInventoryFacade().deleteMedicineByItemName(medicineList);
     }
-    private boolean deleteInventory() {
-        List<Long> medicineIds = getSelectedMedicines().stream()
-                .map(Medicine::getInventoryId)
-                .toList();
-        List<Medicine> filteredMedicines = medicineList.stream()
-                .filter(med -> medicineIds.contains(med.getInventoryId()))
-                .toList();
-        return inventoryManagementApplication.getMedicineInventoryFacade().deleteMedicineByItemName(filteredMedicines);
-    }
-
     /**
      * this method handles the action triggered when the remove button is clicked.
      * @param actionEvent the event triggered by the confirm button click
@@ -331,8 +321,6 @@ public class AddInventoryController implements Initializable {
         }
         if(getSelectedMedicines().size() == 1 ) {
             deleteMedicine();
-            System.out.println("deleted Medicine : " + deleteMedicine());
-            deleteInventory();
             Dialog dialog = new Dialog();
             dialog.setTitle("Notification");
             ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
