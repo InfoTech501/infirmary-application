8f0a2a585766e8f18243c53ec428ae1239ad1999
NNeonael <neonael.angcaya.nazareno@gmail.com>
2025-09-05 18:28:00 +0800

    IA-68 Delete LowStock Item Remain

--- src/main/java/com/rocs/infirmary/application/controller/inventory/InventoryController.java
+++ src/main/java/com/rocs/infirmary/application/controller/inventory/InventoryController.java
@@ -1,6 +1,7 @@
 package com.rocs.infirmary.application.controller.inventory;
 
 import com.rocs.infirmary.application.controller.helper.ControllerHelper;
+import com.rocs.infirmary.application.controller.lowstock.helper.LowStockAlertHelper;
 import com.rocs.infirmary.application.data.model.inventory.medicine.Medicine;
 import com.rocs.infirmary.application.module.inventory.management.application.InventoryManagementApplication;
 import javafx.collections.FXCollections;
@@ -116,6 +117,7 @@ public class InventoryController implements Initializable {
      * this method handles the refresh functionality for inventory table
      ***/
     public void refresh() {
+        LowStockAlertHelper.checkLowStockAndShowAlert();
         List<Medicine> medicineList = inventoryManagementApplication.getMedicineInventoryFacade().getAllMedicine();
         for (Medicine med : medicineList) {
             if (med.isSelectedProperty() == null) {
